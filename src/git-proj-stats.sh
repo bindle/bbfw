1c3e4e0ab5c104b34402b54c7ce895c221a55940
David M. Syzdek <syzdek@bindlebinaries.com>
2011-12-17 13:30:37 -0900

    Updating script with license and new template file.

--- src/git-proj-stats.sh
+++ src/git-proj-stats.sh
@@ -1,9 +1,44 @@
 #!/bin/sh
+#
+#   Bindle Binaries Tools
+#   Copyright (C) 2011 Bindle Binaries <syzdek@bindlebinaries.com>.
+#
+#   @BINDLE_BINARIES_BSD_LICENSE_START@
+#
+#   Redistribution and use in source and binary forms, with or without
+#   modification, are permitted provided that the following conditions are
+#   met:
+#
+#      * Redistributions of source code must retain the above copyright
+#        notice, this list of conditions and the following disclaimer.
+#      * Redistributions in binary form must reproduce the above copyright
+#        notice, this list of conditions and the following disclaimer in the
+#        documentation and/or other materials provided with the distribution.
+#      * Neither the name of Bindle Binaries nor the
+#        names of its contributors may be used to endorse or promote products
+#        derived from this software without specific prior written permission.
+#
+#   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+#   IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+#   THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+#   PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BINDLE BINARIES BE LIABLE FOR
+#   ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+#   DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+#   SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#   CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+#   LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+#   OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+#   SUCH DAMAGE.
+#
+#   @BINDLE_BINARIES_BSD_LICENSE_END@
+#
+#   git-proj-stats.sh - script for generating development stats on active projects
+#
 
 REPODIR=/pub/scm
 SRCDIR=/pub/src
 
-TEMPLATE=/tmp/mail-template.$$
+TEMPLATE=/tmp/mail-project-stats.$$
 DATE=`date +%Y-%m-%d`
 
 
@@ -142,4 +177,6 @@ if test "x${SENTRESULTS}" == "xNO";then
    cat ${TEMPLATE}
 fi
 
-rm -f ${TEMPLATE}
+rm -f /tmp/mail-project-stats.*
+
+# end of script
